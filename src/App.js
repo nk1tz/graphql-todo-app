d30ba07734c27a8007e888512a83d7d9d834ce5c
Nathaniel Kitzke <nkitzke@shutterstock.com>
2018-04-02 12:47:35 -0400

    first graphql query

--- src/App.js
+++ src/App.js
@@ -4,21 +4,38 @@ import { Query, Mutation } from 'react-apollo'
 import logo from './logo.svg'
 import './App.css'
 
+const GET_TODOS = gql`
+  {
+    allTodoes {
+      id
+      text
+      completed
+    }
+  }
+`
+
+const Loading = () => <span>Loading...</span>
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Todo list</h1>
         </header>
-        <div>Signup</div>
+        <Query query={GET_TODOS}>
+          {({ data, error, loading }) =>
+            loading ? <Loading /> : data.allTodoes.map(todo => <div key={todo.id}>{todo.text}</div>)
+          }
+        </Query>
+        {/* <div>Signup</div>
         <span>
           Email<input type="text" />
         </span>
         <span>
           Password<input type="password" />
-        </span>
+        </span> */}
       </div>
     )
   }
